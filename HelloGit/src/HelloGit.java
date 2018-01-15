daae7565c9ff8b9cf95ce838f9ef5e37805b7a28
admin <admin@example.com>
2018-01-15 18:15:21 +0530

    For testing commit

---
+++ HelloGit/src/HelloGit.java
@@ -0,0 +1,10 @@
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		//Commented now
+		System.out.println("Hello Git ..!!!");
+		System.out.println("Modififed now...!!!");
+	}
+
+}
