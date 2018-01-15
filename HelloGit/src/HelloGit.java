f1cd218c1881f1d7cc25f65a99325c26a3e1a7ba
admin <admin@example.com>
2018-01-15 18:16:30 +0530

    For testing

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -2,7 +2,7 @@
 public class HelloGit {
 
 	public static void main(String[] args) {
-		//Commented now
+		//Commented now as shan
 		System.out.println("Hello Git ..!!!");
 		System.out.println("Modififed now...!!!");
 	}
