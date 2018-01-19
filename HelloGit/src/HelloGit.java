b4e88031ddf222eba5fb4a0dcf03f586967c0e51
admin <admin@example.com>
2018-01-19 11:20:36 +0530

    updated with new sdk

--- HelloGit/src/HelloGit.java
+++ HelloGit/src/HelloGit.java
@@ -1,11 +1,26 @@
+import java.util.Properties;
 
 public class HelloGit {
 
+	
+	private static String s = new String("Shan");
+	static String b = s.toString();
+	static HelloGit sf = new HelloGit();
+	static Properties p = new Properties();
+
+	
 	public static void main(String[] args) {
 		//Commented now for testing.
 		System.out.println("Hello Git ..!!!");
 		System.out.println("Modififed now...!!!");
-		System.out.println("Changes ...");//shan
+		System.out.println("Changes ...");//shan changed
+	}
+	
+	void meth()
+	
+	{
+		sf.toString();
+		p.getProperty("testing");
 	}
 
 }
